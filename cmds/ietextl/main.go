19b5d0bbc7cf6ab55f610ac342cd4d0610815f9b
xplshn <musicslave66@murena.io>
2024-07-15 19:54:03 -0300

    Works!

--- cmds/ietextl/main.go
+++ cmds/ietextl/main.go
@@ -4,46 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
-	"strconv"
-	"strings"
-
-	"github.com/xplshn/ietext/pkg/common"
 )
 
-type Line struct {
-	Number int
-	Text   string
-}
-
 func main() {
-	var lines []Line
 	scanner := bufio.NewScanner(os.Stdin)
+	lineNumber := 10 // Starting line number
+
+	// Read from standard input
 	for scanner.Scan() {
 		line := scanner.Text()
-		if len(line) >= 2 && line[:2] == "00" { // Special case for line continuation
-			lines[len(lines)-1].Text += "\n" + line[2:]
-		} else if len(line) >= 2 && strings.HasPrefix(line, "0") {
-			number, err := strconv.Atoi(line[:2])
-			if err != nil {
-				fmt.Println("Error parsing line number:", err)
-				continue
-			}
-			text := strings.TrimSpace(line[2:])
-			lines = append(lines, Line{Number: number, Text: text})
-		} else {
-			fmt.Println("Skipping invalid line:", line)
-		}
+
+		// Prepend the line number to the line
+		fmt.Printf("%02d %s\n", lineNumber, line)
+
+		// Increment the line number by 10 for the next line
+		lineNumber += 10
 	}
 
-	sort.Slice(lines, func(i, j int) bool {
-		return lines[i].Number < lines[j].Number
-	})
-
-	for _, line := range lines {
-		mLine := fmt.Sprintf("%02d %s\n", line.Number, line.Text)
-		if common.IsValidLine(mLine) {
-			fmt.Print(mLine)
-		}
+	// Check for errors during scanning
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 }
